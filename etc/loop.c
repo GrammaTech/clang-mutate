353fa95e42ac19acec09253c879cce3c1c0f6a1b
Jason Ruchti <jruchti@grammatech.com>
2015-10-16 14:15:52 -0400

    Updating loop.c test file for more interesting AST Parse.  ok @self IP:VET-TA2

--- etc/loop.c
+++ etc/loop.c
@@ -2,11 +2,12 @@
 
 int main(int argc, char *argv[])
 {
-  int i;
+  int i=0,
+      j=10;
 
-  for(i=0; i<10; i++)
+  for(i=0,j=10; i<10,j>0; i++,j--)
     {
-      printf("hello %d\n", i);
+      printf("hello %d %d\n", i, j);
     }
 
   return 0;
