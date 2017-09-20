dfee93be6b4e1d84e4ba9b3e5f5bd34b831d3d97
Rebecca Swords <rswords@grammatech.com>
2017-09-20 16:12:58 -0500

    Update scripts used in README examples.
    * Fix syntax warning in etc/loop.c
    * Update perforate-loops and strings-to to use value1 instead of value
    * Fix bugs in perforate-loops that didn't handle multiple unary op
    occurrences or use of -- correctly.
    IP:BugInjector

--- etc/loop.c
+++ etc/loop.c
@@ -5,7 +5,7 @@ int main(int argc, char *argv[])
   int i=0,
       j=10;
 
-  for(i=0,j=10; i<10,j>0; i++,j--)
+  for(i=0,j=10; i<10 && j>0; i++,j--)
     {
       printf("hello %d %d\n", i, j);
     }
